cdfef36cdd5801502dfa4d769e49d107608d47a1
James Fickel <jamesfickel@gmail.com>
2015-06-01 08:20:11 -0700

    Added month helper method to Week class.

--- addon/models/week.js
+++ addon/models/week.js
@@ -27,14 +27,22 @@ let Week = Ember.Object.extend({
     }
   },
 
-  previous() {
+  previous(events) {
     let previousWeek = moment(this.get('_momentDate')).subtract(1, 'weeks');
-    return Week.create({ date: previousWeek.format('YYYY-MM-DD') });
+    return Week.create({ date: previousWeek.format('YYYY-MM-DD'), events: events });
   },
 
-  next() {
+  next(events) {
     let nextWeek = moment(this.get('_momentDate')).add(1, 'weeks');
-    return Week.create({ date: nextWeek.format('YYYY-MM-DD') });
+    return Week.create({ date: nextWeek.format('YYYY-MM-DD'), events: events });
+  },
+
+  month(events) {
+    // we probably want to set @month to the month instance if this week was created
+    // by the Month class — otherwise, just create a new month instance
+    if (this.date) {
+      return Month.create({ date: this.date, events: events });
+    }
   },
 
   _collectEvents(events, date) {
