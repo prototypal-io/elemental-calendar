4ff176da77d225e8a2fe3569c6ef788fe492f2ef
Kristofer Joseph <kristoferjoseph@gmail.com>
2015-06-02 16:18:32 -0700

    Adds flex based calendar styling

--- addon/components/el-monthly-calendar.js
+++ addon/components/el-monthly-calendar.js
@@ -4,6 +4,7 @@ import layout from '../templates/components/el-monthly-calendar';
 
 export default Ember.Component.extend({
   layout: layout,
+  tagName: '',
 
   weeks: Ember.computed('date', function() {
     if (this.date) {
